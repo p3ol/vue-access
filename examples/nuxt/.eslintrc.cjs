1f78ba3b108a40aef27df8076495191f1095c588
Ugo Stephant <ugo@poool.fr>
2024-10-11 08:55:49 +0200

    chore: fix eslint config

--- examples/nuxt/.eslintrc.cjs
+++ examples/nuxt/.eslintrc.cjs
@@ -1,11 +1,9 @@
 /* eslint-env node */
 const OFF = 0;
-const WARNING = 1;
-const ERROR = 2;
 
 module.exports = {
   extends: [
-    '../.eslintrc.cjs',
+    '../../.eslintrc.cjs',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
